08248f2d3db5bc9d362e247c0e2ed817c9f33e61
Farhad hsni <farhad.hsni@gmail.com>
2016-10-05 17:29:48 +0330

    Add challeng no. 291, Wherefore art thou

---
+++ test/291_whatIsInAName_spec.js
@@ -0,0 +1,22 @@
+const whatIsInAName = require("../291_whatIsInAName.js");
+const assert = require("chai").assert;
+
+describe.only("291_whatIsInAName", () => {
+  it("returns an array of all objects that have matching property and value pairs", () => {
+    assert.deepEqual(whatIsInAName(
+      [{ first: "Romeo", last: "Montague" },
+       { first: "Mercutio", last: null },
+       { first: "Tybalt", last: "Capulet" }],
+      { last: "Capulet" }), [{ first: "Tybalt", last: "Capulet" }]);
+
+    assert.deepEqual(whatIsInAName([{ a: 1 }, { a: 1 }, { a: 1, b: 2 }],
+                                   { a: 1 }),
+                     [{ a: 1 }, { a: 1 }, { a: 1, b: 2 }]);
+    assert.deepEqual(whatIsInAName([{ a: 1, b: 2 }, { a: 1 }, { a: 1, b: 2, c: 2 }],
+                                   { a: 1, b: 2 }),
+                     [{ a: 1, b: 2 }, { a: 1, b: 2, c: 2 }]);
+    assert.deepEqual(whatIsInAName([{ a: 1, b: 2 }, { a: 1 }, { a: 1, b: 2, c: 2 }],
+                                   { a: 1, c: 2 }),
+                     [{ a: 1, b: 2, c: 2 }]);
+  });
+});
